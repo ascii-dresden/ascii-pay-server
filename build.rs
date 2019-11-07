dbf331785ec84f333419b081993302690e59421f
Felix Wittwer <dev@felixwittwer.de>
2019-11-07 11:27:03 +0100

    Add buildscript for scss and sass files w/ automatic rerun

---
+++ build.rs
@@ -0,0 +1,56 @@
+use sass_rs;
+use std::fs::{self, File};
+use std::io;
+use std::io::Write;
+use std::path::Path;
+
+fn process_files(path: &Path) -> io::Result<()> {
+    println!("Checking path {}", path.display());
+    if path.is_dir() {
+        // recurse and register paths as rebuild conditions
+        println!("cargo:rerun-if-changed={}", path.display());
+
+        for entry in fs::read_dir(path)? {
+            let entry = entry?;
+            let path = entry.path();
+            process_files(&path)?;
+        }
+    } else {
+        // compile any *.sass and *.scss files
+        if let Some(ext) = path.extension() {
+            if ext == "sass" || ext == "scss" {
+                // register any scss and sass files found as triggers for a rebuild
+                println!("cargo:rerun-if-changed={}", path.display());
+
+                // run the compilation abd write to css
+                let output_path = path.with_extension("css");
+                match sass_rs::compile_file(path, sass_rs::Options::default()) {
+                    Ok(compiled) => {
+                        let mut css_file = File::create(output_path)?;
+                        css_file.write_all(compiled.as_bytes())?;
+                    }
+                    Err(msg) => panic!("Compilation of sass failed: {}", msg),
+                }
+            } else {
+                println!("Skipping non-sass file with: {}", path.display());
+            }
+        }
+    }
+
+    Ok(())
+}
+
+fn main() {
+    // build any sass files found in the templates folder
+    let sass_path = Path::new("static/templates/");
+    if !sass_path.exists() {
+        panic!(
+            "The static template path at {} could not be found. Please make sure it exists.",
+            sass_path.canonicalize().expect("Invalid path").display()
+        );
+    } else {
+        if let Err(e) = process_files(sass_path) {
+            panic!("{}", e);
+        }
+    }
+}
