97c73f8460afb6e55a86bae96c1725db8630ebb4
Lars Westermann <lars-westermann@live.de>
2020-02-11 15:55:29 +0100

    Improve monthly report mail
    - Split money spent value
    - Add transaction overview
    
    wip #26

--- src/web/admin/cron.rs
+++ src/web/admin/cron.rs
@@ -3,6 +3,20 @@ use crate::core::mail::send_report_mail;
 use crate::core::{transactions, Account, DbConnection, Pool, ServiceError, ServiceResult};
 use actix_web::{web, HttpRequest, HttpResponse};
 use chrono::{Datelike, Local};
+use std::cmp::max;
+
+fn pad_left(s: &str, width: usize) -> String {
+    let mut result = String::with_capacity(width);
+    result.push_str(&" ".repeat(width - s.len()));
+    result.push_str(s);
+    result
+}
+fn pad_right(s: &str, width: usize) -> String {
+    let mut result = String::with_capacity(width);
+    result.push_str(s);
+    result.push_str(&" ".repeat(width - s.len()));
+    result
+}
 
 /// Prepares a transaction report for a given user. If the user did not do any transactions in a month, `None` is returned.
 fn generate_report(
@@ -29,19 +43,101 @@ fn generate_report(
 
     let list = transactions::get_by_account(&conn, &account, &start, &end)?;
 
-    let total_spent = list.iter().fold(0, |acc, ta| acc + ta.total);
-    if total_spent == 0 {
+    let total_down = list
+        .iter()
+        .filter(|ta| ta.total < 0)
+        .fold(0, |acc, ta| acc - ta.total) as f32
+        / 100.0;
+    let total_up = list
+        .iter()
+        .filter(|ta| ta.total > 0)
+        .fold(0, |acc, ta| acc + ta.total) as f32
+        / 100.0;
+    if total_down == 0.0 && total_up == 0.0 {
         return Ok(None);
     }
 
-    // TODO: Balance before & after, overview over transactions
+    let start_balance = list[list.len() - 1].before_credit as f32 / 100.0;
+    let end_balance = list[0].after_credit as f32 / 100.0;
+
+    let trans: Vec<(String, String, String)> = list
+        .into_iter()
+        .map(|ta| {
+            let c1 = ta.date.format("%d.%m.%Y - %H:%M").to_string();
+            let c2 = if let Ok(prods) = ta.get_products(&conn) {
+                let mut prods_str = prods
+                    .iter()
+                    .map(|p| format!("{} x {}", p.1, p.0.name))
+                    .collect::<Vec<String>>()
+                    .join(", ");
+                if prods_str.len() > 30 {
+                    let mut help = String::with_capacity(30);
+                    help.push_str(&prods_str[0..27]);
+                    help.push_str("...");
+                    prods_str = help;
+                }
+                prods_str
+            } else {
+                "".to_owned()
+            };
+            let c3 = format!("{:.2}€", ta.total as f32 / 100.0);
+            (c1, c2, c3)
+        })
+        .collect();
+
+    let table_head = ("Date".to_owned(), "Products".to_owned(), "Total".to_owned());
+
+    let (w1, w2, w3) = trans.iter().fold(
+        (table_head.0.len(), table_head.1.len(), table_head.2.len()),
+        |(w1, w2, w3), (c1, c2, c3)| (max(c1.len(), w1), max(c2.len(), w2), max(c3.len(), w3)),
+    );
+
+    let mut table: Vec<String> = trans
+        .into_iter()
+        .map(|(c1, c2, c3)| {
+            format!(
+                " {} | {} | {}",
+                pad_right(&c1, w1),
+                pad_right(&c2, w2),
+                pad_left(&c3, w3)
+            )
+        })
+        .collect();
+
+    table.insert(
+        0,
+        format!(
+            " {} | {} | {}",
+            pad_right(&table_head.0, w1),
+            pad_right(&table_head.1, w2),
+            pad_right(&table_head.2, w3)
+        ),
+    );
+
+    table.insert(
+        1,
+        format!(
+            "-{}-|-{}-|{}",
+            "-".repeat(w1),
+            "-".repeat(w2),
+            "-".repeat(w3)
+        ),
+    );
+
+    let table = table.join("\n");
 
     let subject_line = format!("[ascii pay] Your report for {}", start.format("%m/%Y"));
     let message = format!("Hey {user},
 
-this is your monthly transaction report for {month} from the ascii-pay-system.
+this is your monthly transaction report for {month} from the ascii pay system.
+ 
+Total spent:           {total_down:5.2}€
+Total charged to card: {total_up:5.2}€
 
-Last month, you spent {total}€.
+Start balance: {start_balance:5.2}€
+End balance:   {end_balance:5.2}€
+
+{table}
 
 The Ascii Pay System
 
@@ -51,7 +147,12 @@ You are receiving this email because you opted in to receive monthly reports abo
 If you don't want to receive these mails anymore, you can change your settings in the ascii pay system.",
         user = account.name,
         month = start.format("%B %Y"),
-        total = total_spent);
+        total_down = total_down,
+        total_up = total_up,
+        start_balance = start_balance,
+        end_balance = end_balance,
+        table = table,
+    );
 
     Ok(Some((subject_line, message)))
 }
