132cbfbf2c2d3749d23872b954f95aa8827cdaa0
revol-xut <revol-xut@protonmail.com>
2022-02-25 22:11:22 +0100

    add flake.nix

---
+++ derivation.nix
@@ -0,0 +1,19 @@
+{ naersk, src, lib, pkg-config, protobuf, gcc, cmake, openssl, libpqxx, libiconv, postgresql, git }:
+
+naersk.buildPackage {
+  pname = "ascii-pay-server";
+  version = "0.1.0";
+
+  inherit src;
+
+  cargoSha256 = lib.fakeSha256;
+
+  nativeBuildInputs = [ pkg-config protobuf gcc cmake ];
+  buildInputs = [ openssl libpqxx libiconv postgresql git ];
+
+  meta = with lib; {
+    description = "Rust server which handles the transactions of the ascii-pay system.";
+    homepage = "https://github.com/ascii-dresden/ascii-pay-server.git";
+    license = with licenses; [ mit ];
+  };
+}
---
+++ flake.nix
@@ -0,0 +1,18 @@
+{
+  inputs.nixpkgs.url = github:NixOS/nixpkgs/nixos-21.11;
+
+  inputs.naersk = {
+    url = github:nix-community/naersk;
+    inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = { self, nixpkgs, naersk, ... }@inputs: 
+  let
+    pkgs = import nixpkgs { system = "x86_64-linux"; };
+  in {
+    defaultPackage."x86_64-linux" = pkgs.callPackage ./derivation.nix {
+      src = ./.;
+      naersk = naersk.lib.x86_64-linux;
+    };
+  };
+}
