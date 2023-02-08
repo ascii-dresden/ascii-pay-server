b6992e89e8be744d124fbaef9862e176dce0636e
Lars Westermann <git@lars-westermann.de>
2023-02-08 18:53:10 +0100

    Improve integration test script

--- README.md
+++ README.md
@@ -2,8 +2,8 @@
 
 ## Run integration tests
 
+Integration test are based on a postman collection and executed via `newman`. All test are run within `docker`/`docker compose` so no additional dependencies are necessary.
+
 ```bash
-docker compose -f docker-compose.test.yml up -d --build
-newman run ascii-pay-tests.postman_collection.json
-docker compose -f docker-compose.test.yml down
+./tests.sh
 ```
---
+++ tests.sh
@@ -0,0 +1,35 @@
+#/bin/bash
+
+
+function onExit {
+    if [ "$?" != "0" ]; then
+        echo "Tests failed";
+        echo;
+        docker compose -f docker-compose.test.yml down -v;
+        exit 1;
+    else
+        echo "Tests passed";
+        echo;
+        docker compose -f docker-compose.test.yml down -v;
+    fi
+}
+
+trap onExit EXIT;
+
+previous_command=`docker images | grep asciipay/newman`
+cmd=$previous_command ret=$?
+if [ $ret -ne 0 ]; then
+    docker build -f docker/newman.Dockerfile -t asciipay/newman:latest .;
+fi
+
+# Stop on first error
+set -e;
+
+docker compose -f docker-compose.test.yml up -d;
+
+sleep 1;
+
+docker run -v $(pwd)/collections:/etc/newman \
+        --network=ascii-pay-server-newman-test-network \
+        -it --rm asciipay/newman:latest \
+        run ascii-pay-tests.postman_collection.json --env-var "base_url=http://server:3000"
