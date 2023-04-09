02eecd87ac8655637f9e77491331a89cbdcac024
Jakob Krebs <jakob@krbs.me>
2023-04-09 18:33:18 +0200

    readd hydra job

--- flake.nix
+++ flake.nix
@@ -18,41 +18,41 @@
     };
   };
 
-  outputs = inputs@{ self, nixpkgs, flake-utils, crane, rust-overlay}:
+  outputs = inputs@{ self, nixpkgs, flake-utils, crane, rust-overlay }:
     flake-utils.lib.eachDefaultSystem (system:
-    let
-      pkgs = import nixpkgs {
+      let
+        pkgs = import nixpkgs {
           inherit system;
           overlays = [ (import rust-overlay) ];
         };
         lib = pkgs.lib;
-      rust = pkgs.rust-bin.nightly.latest.default.override { };
+        rust = pkgs.rust-bin.nightly.latest.default.override { };
 
-      craneLib = (crane.mkLib pkgs).overrideToolchain rust;
-      package = pkgs.callPackage ./derivation.nix { craneLib = craneLib; };
-    in
-    rec {
-    doCheck=false;
-    packages = {
-      ascii-pay-server = package;
-      default = package;
-    };
-    apps = {
-      ascii-pay-server = flake-utils.lib.mkApp { drv = packages.ascii-pay-server; };
-      default = apps.ascii-pay-server;
-    };
-  }) // {
-    nixosModules = rec {
-        default = ascii-pay-server;
-        ascii-pay-server = import ./nixos-module;
-    };
-      overlay = final: prev: {
-        ascii-pay-server = self.packages.ascii-pay-server;
-        ascii-pay-server-src = ./.;
+        craneLib = (crane.mkLib pkgs).overrideToolchain rust;
+        package = pkgs.callPackage ./derivation.nix { craneLib = craneLib; };
+      in rec {
+        doCheck = false;
+        packages = {
+          ascii-pay-server = package;
+          default = package;
+        };
+        apps = {
+          ascii-pay-server =
+            flake-utils.lib.mkApp { drv = packages.ascii-pay-server; };
+          default = apps.ascii-pay-server;
+        };
+
+        hydraJobs = {
+          ascii-pay-server."x86_64-linux" = packages.ascii-pay-server;
+        };
+      }) // {
+        nixosModules = rec {
+          default = ascii-pay-server;
+          ascii-pay-server = import ./nixos-module;
+        };
+
+        overlays.default = final: prev: {
+          inherit (self.packages.${prev.system}) ascii-pay-server;
+        };
       };
-    overlays.default = final: prev: {
-      inherit (self.packages.${prev.system})
-      ascii-pay-server;
-    };
-  };
 }
