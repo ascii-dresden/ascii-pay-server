d9da016409ce8c469d88add798da734fa7c1c80d
Jakob Krebs <jakob@krbs.me>
2023-04-09 14:56:40 +0200

    add flake to build nix derivation

---
+++ flake.nix
@@ -0,0 +1,63 @@
+{
+  description = "Build ascii-pay-server";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+    crane = {
+      url = "github:ipetkov/crane";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    flake-utils.url = "github:numtide/flake-utils";
+
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        flake-utils.follows = "flake-utils";
+      };
+    };
+  };
+
+  outputs = { self, nixpkgs, crane, flake-utils, rust-overlay, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ (import rust-overlay) ];
+        };
+        lib = pkgs.lib;
+
+        rustWithWasiTarget = pkgs.rust-bin.nightly.latest.default.override { };
+
+        craneLib = (crane.mkLib pkgs).overrideToolchain rustWithWasiTarget;
+
+        filter = path: _type: builtins.match ".*md$|.*sql$" path != null;
+        markdownOrSQLOrCargo = path: type:
+          (filter path type) || (craneLib.filterCargoSources path type);
+
+        ascii-pay-server = craneLib.buildPackage {
+          src = lib.cleanSourceWith {
+            src = craneLib.path ./.;
+            filter = markdownOrSQLOrCargo;
+          };
+
+          doCheck = false;
+
+        };
+      in {
+        checks = { inherit ascii-pay-server; };
+
+        packages.default = ascii-pay-server;
+
+        apps.default = flake-utils.lib.mkApp { drv = "ascii-pay-server"; };
+
+        devShells.default = pkgs.mkShell {
+          inputsFrom = builtins.attrValues self.checks.${system};
+
+          # Extra inputs can be added here
+          nativeBuildInputs = with pkgs; [ ];
+        };
+      });
+}
