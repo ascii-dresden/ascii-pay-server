e2e4e8e6c736d36fb2dff4f4855b34007264774e
Benno Fünfstück <benno.fuenfstueck@gmail.com>
2023-07-07 15:47:32 +0200

    nix: install AsciiPayCard.pass to $out

--- derivation.nix
+++ derivation.nix
@@ -1,17 +1,17 @@
-{ craneLib, src, lib }:
+{ craneLib, src, lib, openssl, pkgconf }:
 
-craneLib.buildPackage {
+(craneLib.buildPackage {
   pname = "ascii-pay-server";
   version = "2.0.0";
 
   src = lib.cleanSourceWith {
     src = craneLib.path ./.;
     filter = path: type:
-      ((path: _type: builtins.match ".*md$|.*sql$" path != null) path type) || (craneLib.filterCargoSources path type);
+      ((path: _type: builtins.match ".*md$|.*sql$|.*/AsciiPayCard\\.pass(/.*|$)" path != null) path type) || (craneLib.filterCargoSources path type);
   };
 
-  nativeBuildInputs = [ ];
-  buildInputs = [ ];
+  nativeBuildInputs = [ pkgconf ];
+  buildInputs = [ openssl ];
   doCheck = false;
   meta = with lib; {
     description =
@@ -19,4 +19,15 @@ craneLib.buildPackage {
     homepage = "https://github.com/ascii-dresden/ascii-pay-server.git";
     license = with licenses; [ mit ];
   };
-}
+})
+
+# we use override attrs because setting this on the derivation itself
+# affects both the final derivation and the -deps derivation,
+# but $src/AsciiPayCard only exists in the final derivation
+.overrideAttrs (final: prev: {
+  postInstall = ''
+    mkdir -p $out/share/ascii-pay-server
+    cp -r $src/AsciiPayCard.pass $out/share/ascii-pay-server/
+    chmod -R a+w $out/share/ascii-pay-server
+  '';
+})
